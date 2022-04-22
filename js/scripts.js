e26a1c372d1ad108591f45087c50d705ee03be22
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2022-04-22 22:47:57 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+(function() {
+  let form = document.querySelector('#contact-form');
+  let emailInput = document.querySelector('#contact-email');
+  
+  function validateEmail() {
+    let value = emailInput.value;
+    if (!value) {
+      showErrorMessage(emailInput, 'Email is a required field.');
+      return false;
+    }
+
+    if (value.indexOf('@') === -1 || value.indexOf('.') === -1) {
+      showErrorMessage(emailInput, 'Please enter a valid email address.');
+      return false;
+    }
+
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+  
+
+
+  function showErrorMessage(input, message) {
+    let container = input.parentElement; // The .input-wrapper
+
+    // Remove an existing error
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
+    }
+
+    // Now add the error if the message isn’t empty
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  } 
+
+  emailInput.addEventListener('input', validateEmail);
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    if (validateEmail()) {
+      alert('Success!'); 
+    }
+  })
+
+})();
